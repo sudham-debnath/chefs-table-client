e83917ee8eaca3552e5308d457d7edde6fcce20f
SudhamDebnath <sudham15-12854@diu.edu.bd>
2023-05-03 12:21:23 +0600

    Create project with UI

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAoTJVG7_hbfPNELUpNqcM1r9cNuEOI1d4",
+  authDomain: "chefs-table-client-7ac47.firebaseapp.com",
+  projectId: "chefs-table-client-7ac47",
+  storageBucket: "chefs-table-client-7ac47.appspot.com",
+  messagingSenderId: "446903449862",
+  appId: "1:446903449862:web:c845796b08dfa01e520687"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
